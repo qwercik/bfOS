96320b52ac0b837458884103c5adf8d5fb6c7716
Eryk Andrzejewski <erykandrzejewski2000@gmail.com>
2017-07-08 23:40:40 +0200

    Little brainfuck intepreter optimization. Update .gitignore.

--- loader/src/bf.asm
+++ loader/src/bf.asm
@@ -82,8 +82,8 @@ bf_loop_start:
 		cmp cx, 0
 		jne .check_characters
 		cmp al, ']'
-		je .end
-		
+		jmp bf_interpreter
+
 		.check_characters:
 			cmp al, '['
 			je .start_of_loop_char
@@ -99,13 +99,18 @@ bf_loop_start:
 				dec cx
 				jmp .ptr_zero
 		
-	.end:
+bf_loop_end:
+	mov bl, byte [es:di]
+	cmp bl, 0
+	je .ptr_zero
+	
+	.ptr_non_zero:
+		pop si
 		jmp bf_interpreter
 
-bf_loop_end:
-	pop si
-	jmp bf_interpreter
-
+	.ptr_zero:
+		add sp, 2
+		jmp bf_interpreter
 
 %include "screen.asm"
 
